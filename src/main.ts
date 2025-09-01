051e3f6a1fe61c8b347db84b267c8b1a9e024459
Deepak Jonnalagadda <deepak.jonnalagadda@storable.com>
2025-09-01 18:42:53 +0530

    feat: add versioning, global prefix & install deps

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,8 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.setGlobalPrefix('api');
+  app.enableVersioning();
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
